5cfc30427d5e070d7ffcc5ceda1ef385844e8ff8
Kartik <kartik.mathur10@gmail.com>
2025-08-03 18:20:34 +0530

    L14-DOM

---
+++ L14-DOM/1_eventListener/2_ListenerDifference.js
@@ -0,0 +1,21 @@
+let button = document.querySelector('button');
+const firstCallback = () => {
+    console.log("First Callback function for click");
+}
+button.addEventListener('click',firstCallback )
+
+const secondCallback = () => {
+    console.log("Second Callback function for click");
+}
+
+button.addEventListener('click', secondCallback)
+
+button.removeEventListener('click', firstCallback)
+
+// button.onclick = () => {
+//     console.log("First Callback function for click");
+// }
+
+// button.onclick = () => {
+//     console.log("Second Callback function for click");
+// }
