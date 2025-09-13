4ab86ebb53182ebdc8e182dfb49041704bdbc30f
Kartik <kartik.mathur10@gmail.com>
2025-09-13 13:05:33 +0530

    File handlling and file dependency

--- L21-Databases/1_FileHandling/Todos.js
+++ L21-Databases/1_FileHandling/Todos.js
@@ -1,4 +1,78 @@
-class Todos {
-    
+const fs = require('node:fs/promises');
+const path = require('path');
+const { v4: uuid } = require('uuid');
+let filePath = path.join(__dirname, 'database', 'todos.json');
+async function getData() {
+    try {
+        let data = await fs.readFile(filePath, {
+            encoding: 'utf-8'
+        });
+
+        return JSON.parse(data);
+    } catch (error) {
+        throw new Error("Unable to read file")
+    }
 }
 
+async function writeData(data) {
+    try {
+        await fs.writeFile(filePath, JSON.stringify(data));
+        return data;
+    } catch (error) {
+        throw new Error("Unable to read file")
+    }
+}
+
+class Todos {
+    static async addTodo(task, description) {
+        try {
+            let todos = await getData();
+            console.log(todos);
+            let newTodo = {
+                id: uuid(),
+                task,
+                description
+            }
+            // console.log(newTodo)
+            todos.push(newTodo);
+            console.log(todos);
+            await writeData(todos);
+            return "newtodo added successfully";
+        } catch (error) {
+            throw new Error("Unable to add a newTodo");
+        }
+    }
+
+    static async getTodos() {
+        try {
+            let todos = await getData();
+            return todos;
+        } catch (error) {
+            throw new Error("Unable to fetch all todos");
+        }
+    }
+
+    static async getTodo(id) {
+        // 
+        try {
+            let todos = await getData();
+            todos = todos.filter(todo => todo.id == id);
+            return todos[0];
+        } catch (error) {
+            throw new Error("Unable to fetch single todos");
+        }
+    }
+
+    static async deleteTodo(id) {
+        try {
+            let todos = await getData();
+            todos = todos.filter(todo => todo.id != id);
+            await writeData(todos);
+            return "Deleted todo successfully";
+        } catch (error) {
+            throw new Error("Unable to delete todo");
+        }
+    }
+}
+
+module.exports = Todos;
--- L21-Databases/1_FileHandling/app.js
+++ L21-Databases/1_FileHandling/app.js
@@ -3,65 +3,65 @@ const app = express();
 const { v4: uuid } = require('uuid');
 const PORT = 4444;
 const path = require('path');
+const Todos = require('./Todos');
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.urlencoded({ extended: true }));
 
-let todos = [
-    {
-        id: 1,
-        task: "Buy groceries",
-        description: "Pick up milk, eggs, bread, and vegetables from the store."
-    },
-    {
-        id: 2,
-        task: "Finish project report",
-        description: "Complete the final draft of the project report and email it to the manager."
-    },
-    {
-        id: 3,
-        task: "Call the plumber",
-        description: "Schedule an appointment to fix the leaking kitchen sink."
-    },
-    {
-        id: 4,
-        task: "Workout",
-        description: "Do a 30-minute cardio session and some light stretching."
-    },
-    {
-        id: 5,
-        task: "Read a book",
-        description: "Read at least 20 pages of the new novel before bed."
+app.get('/', async (req, res) => {
+    try {
+        let todos = await Todos.getTodos();
+        res.render('index', { todos });
+    } catch (error) {
+        res.render('error', {
+            error
+        })
     }
-];
-
-app.get('/', (req, res) => {
-    res.render('index', { todos });
 })
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', async (req, res) => {
     const { id } = req.params;
-    const data = todos.filter(todo => todo.id == +id);
-    res.render('index', {
-        todos: data
-    })
+    // const data = todos.filter(todo => todo.id == +id);
+    try {
+        let data = await Todos.getTodo(id);
+        res.render('index', {
+            todos: data
+        })
+    } catch (error) {
+        res.render('error', {
+            error
+        })
+    }
 })
 
-app.get('/delete-todo/:id', (req, res) => {
+app.get('/delete-todo/:id', async (req, res) => {
     const { id } = req.params;
-    todos = todos.filter(todo => todo.id != +id);
-    res.redirect('/');
+    // todos = todos.filter(todo => todo.id != +id);
+    try {
+        let msg = await Todos.deleteTodo(id);
+        console.log(msg);
+        res.redirect('/');
+    } catch (error) {
+        res.render('error', {
+            error
+        })
+    }
+
 })
 
-app.post('/todos', (req, res) => {
+app.post('/todos', async (req, res) => {
     const { task, description } = req.body;
-    todos.push({
-        id: uuid(),
-        task,
-        description
-    })
-    console.log(todos);
-    res.redirect('/');
+    try {
+        let msg = await Todos.addTodo(task,description);
+        console.log(msg);
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error)
+        res.render('error', {
+            error
+        })
+    }
 })
 
 
