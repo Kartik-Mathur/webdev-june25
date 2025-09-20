c26c327583ddc72ba9d754bbe79ce837372ccf11
Kartik <kartik.mathur10@gmail.com>
2025-09-20 18:09:46 +0530

    databases

---
+++ L25-databases/customers.data.js
@@ -0,0 +1,27 @@
+[
+    {
+        "_id": { "$oid": "661d6fe5f3e325e3b65e3101" },
+        "name": "Kartik",
+        "email": "kartik@cb.com"
+    },
+    {
+        "_id": { "$oid": "661d6fe5f3e325e3b65e3102" },
+        "name": "Kanak",
+        "email": "kanak@cb.com"
+    },
+    {
+        "_id": { "$oid": "661d6fe5f3e325e3b65e3103" },
+        "name": "Abhishek",
+        "email": "abhishek@cb.com"
+    },
+    {
+        "_id": { "$oid": "661d6fe5f3e325e3b65e3104" },
+        "name": "Monu",
+        "email": "monu@cb.com"
+    },
+    {
+        "_id": { "$oid": "661d6fe5f3e325e3b65e3105" },
+        "name": "Aayush",
+        "email": "aayush@cb.com"
+    }
+]
---
+++ L25-databases/orders.data.js
@@ -0,0 +1,33 @@
+
+[
+  {
+    "_id": { "$oid": "661d7fe5f3e325e3b65e4101" },
+    "item": "Laptop",
+    "customerId": { "$oid": "661d6fe5f3e325e3b65e3101" }
+  },
+  {
+    "_id": { "$oid": "661d7fe5f3e325e3b65e4102" },
+    "item": "Mouse",
+    "customerId": { "$oid": "661d6fe5f3e325e3b65e3102" }
+  },
+  {
+    "_id": { "$oid": "661d7fe5f3e325e3b65e4103" },
+    "item": "Keyboard",
+    "customerId": { "$oid": "661d6fe5f3e325e3b65e3102" }
+  },
+  {
+    "_id": { "$oid": "661d7fe5f3e325e3b65e4104" },
+    "item": "Monitor",
+    "customerId": { "$oid": "661d6fe5f3e325e3b65e3102" }
+  },
+  {
+    "_id": { "$oid": "661d7fe5f3e325e3b65e4105" },
+    "item": "USB Cable",
+    "customerId": { "$oid": "661d6fe5f3e325e3b65e3103" }
+  },
+  {
+    "_id": { "$oid": "661d7fe5f3e325e3b65e4106" },
+    "item": "Charger",
+    "customerId": { "$oid": "661d6fe5f3e325e3b65e3103" }
+  }
+]
